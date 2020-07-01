4b4bcf6cae790e6919ae4138cd075dbc5dfe319f
Maria Gabriella Brodi <50110012+mgbrodi@users.noreply.github.com>
2020-07-01 17:03:37 -0400

    Update install.sh
    Get release number to avoid errors in pushd

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 curl -L https://istio.io/downloadIstio | sh -
-pushd istio-1.6.3
+REL=$(curl -L -s https://api.github.com/repos/istio/istio/releases | \
+                  grep tag_name | sed "s/ *\"tag_name\": *\"\\(.*\\)\",*/\\1/" | \
+                  grep -v -E "(alpha|beta|rc)\.[0-9]$" | sort -t"." -k 1,1 -k 2,2 -k 3,3 -k 4,4 | tail -n 1)
+pushd istio-$REL
 export PATH=$PWD/bin:$PATH
 istioctl install --set profile=demo
 kubectl label namespace default istio-injection=enabled
