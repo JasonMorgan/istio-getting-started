6517378dff8f739c199e088f5795f6ec7d3c5975
Maria Gabriella Brodi <50110012+mgbrodi@users.noreply.github.com>
2020-07-01 17:30:25 -0400

    Update request-routing.sh
    Getting latest release

--- request-routing.sh
+++ request-routing.sh
@@ -3,7 +3,11 @@
 
 ## Route1
 
-kubectl apply -f istio-1.6.3/samples/bookinfo/networking/virtual-service-all-v1.yaml
+REL=$(curl -L -s https://api.github.com/repos/istio/istio/releases | \
+                  grep tag_name | sed "s/ *\"tag_name\": *\"\\(.*\\)\",*/\\1/" | \
+                  grep -v -E "(alpha|beta|rc)\.[0-9]$" | sort -t"." -k 1,1 -k 2,2 -k 3,3 -k 4,4 | tail -n 1)
+                  
+kubectl apply -f istio-$REL/samples/bookinfo/networking/virtual-service-all-v1.yaml
 
 ## Tour around the objects
 kubectl get virtualservices -o yaml
@@ -13,7 +17,7 @@ kubectl get destinationrules -o yaml
 
 ## Route2
 
-kubectl apply -f istio-1.6.3/samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml
+kubectl apply -f istio-$REL/samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml
 
 kubectl get virtualservice reviews
 kubectl get virtualservice reviews -o yaml
@@ -26,4 +30,4 @@ kubectl get virtualservice reviews -o yaml
 
 ## Cleanup
 ### Skip this by default
-kubectl delete -f istio-1.6.3/samples/bookinfo/networking/virtual-service-all-v1.yaml
+kubectl delete -f istio-$REL/samples/bookinfo/networking/virtual-service-all-v1.yaml
