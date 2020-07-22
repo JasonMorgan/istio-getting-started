8af384877e95df67689a794a86ee49e356cecdfe
Jason Morgan <jmorgan@f9vs.com>
2020-07-22 14:32:31 -0400

    Adding a shell setup script.

---
+++ setup_shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export REL=$(curl -L -s https://api.github.com/repos/istio/istio/releases | \
+                  grep tag_name | sed "s/ *\"tag_name\": *\"\\(.*\\)\",*/\\1/" | \
+                  grep -v -E "(alpha|beta|rc)\.[0-9]$" | sort -t"." -k 1,1 -k 2,2 -k 3,3 -k 4,4 | tail -n 1)
+alias bat="bat -l yaml"
+pushd istio-$REL > /dev/null
+export PATH=$PWD/bin:$PATH
+popd > /dev/null
