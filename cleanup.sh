83ec26ffd5e31077776aaeca4e30ced98b155874
Jason Morgan <jmorgan@f9vs.com>
2020-07-22 12:39:05 -0400

    Made it general.

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-istio-1.6.3/samples/bookinfo/platform/kube/cleanup.sh
+istio-*/samples/bookinfo/platform/kube/cleanup.sh
 istioctl manifest generate --set profile=demo | kubectl delete -f -
 kubectl delete namespace istio-system
