5889001b4f7444e4e47c45a87619ab3caef04a7e
Jason Morgan <jmorgan@f9vs.com>
2020-07-22 17:18:47 -0400

    Adding a gcp comment.

--- 02_setup-bookinfo.sh
+++ 02_setup-bookinfo.sh
@@ -9,7 +9,7 @@ kubectl apply -f istio-$REL/samples/bookinfo/networking/bookinfo-gateway.yaml
 kubectl apply -f istio-$REL/samples/bookinfo/networking/destination-rule-all.yaml
 
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
-# export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+# export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}') For GCP
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
 export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
