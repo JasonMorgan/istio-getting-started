b0fe134fc6595c283ee346b4d033257a543a5fe8
Jason Morgan <jmorgan@f9vs.com>
2020-07-22 14:24:09 -0400

    fixing variable version references.

--- 05_fault-injection.sh
+++ 05_fault-injection.sh
@@ -2,12 +2,16 @@
 
 ## Based on the steps here: https://istio.io/latest/docs/tasks/traffic-management/fault-injection/
 
+REL=$(curl -L -s https://api.github.com/repos/istio/istio/releases | \
+                  grep tag_name | sed "s/ *\"tag_name\": *\"\\(.*\\)\",*/\\1/" | \
+                  grep -v -E "(alpha|beta|rc)\.[0-9]$" | sort -t"." -k 1,1 -k 2,2 -k 3,3 -k 4,4 | tail -n 1)
+
 ## If needed:
-kubectl apply -f istio-1.6.3/samples/bookinfo/networking/virtual-service-all-v1.yaml
-kubectl apply -f istio-1.6.3/samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml
+kubectl apply -f istio-$REL/samples/bookinfo/networking/virtual-service-all-v1.yaml
+kubectl apply -f istio-$REL/samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml
 
 ## Inject a delay:
-kubectl apply -f istio-1.6.3/samples/bookinfo/networking/virtual-service-ratings-test-delay.yaml
+kubectl apply -f istio-$REL/samples/bookinfo/networking/virtual-service-ratings-test-delay.yaml
 
 k get virtualservices ratings -o yaml
 
@@ -18,12 +22,12 @@ k get virtualservices ratings -o yaml
 #### Only fixes reviews, ratings stays down.
 
 ### Cleanup (Optional)
-kubectl apply -f istio-1.6.3/samples/bookinfo/networking/virtual-service-all-v1.yaml
-kubectl apply -f istio-1.6.3/samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml
+kubectl apply -f istio-$REL/samples/bookinfo/networking/virtual-service-all-v1.yaml
+kubectl apply -f istio-$REL/samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml
 
 ## Inject HTTP abort
 
-kubectl apply -f istio-1.6.3/samples/bookinfo/networking/virtual-service-ratings-test-abort.yaml
+kubectl apply -f istio-$REL/samples/bookinfo/networking/virtual-service-ratings-test-abort.yaml
 
 ### Now bookinfo loads with no ratings instantly because ratings returns an error
 
@@ -32,4 +36,4 @@ k get virtualservices ratings -o yaml
 
 ### Cleanup
 
-kubectl delete -f istio-1.6.3/samples/bookinfo/networking/virtual-service-all-v1.yaml
+kubectl delete -f istio-$REL/samples/bookinfo/networking/virtual-service-all-v1.yaml
--- 06_traffic-shifting.sh
+++ 06_traffic-shifting.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+REL=$(curl -L -s https://api.github.com/repos/istio/istio/releases | \
+                  grep tag_name | sed "s/ *\"tag_name\": *\"\\(.*\\)\",*/\\1/" | \
+                  grep -v -E "(alpha|beta|rc)\.[0-9]$" | sort -t"." -k 1,1 -k 2,2 -k 3,3 -k 4,4 | tail -n 1)
+
 ## If needed:
-kubectl apply -f istio-1.6.3/samples/bookinfo/networking/virtual-service-all-v1.yaml
-kubectl apply -f istio-1.6.3/samples/bookinfo/networking/virtual-service-reviews-50-v3.yaml
+kubectl apply -f istio-$REL/samples/bookinfo/networking/virtual-service-all-v1.yaml
+
+## Traffic shift
+kubectl apply -f istio-$REL/samples/bookinfo/networking/virtual-service-reviews-50-v3.yaml
 kubectl get virtualservice reviews -o yaml
-kubectl apply -f istio-1.6.3/samples/bookinfo/networking/virtual-service-reviews-v3.yaml
+kubectl apply -f istio-$REL/samples/bookinfo/networking/virtual-service-reviews-v3.yaml
 
 ## Cleanup
-kubectl delete -f istio-1.6.3/samples/bookinfo/networking/virtual-service-all-v1.yaml
+kubectl delete -f istio-$REL/samples/bookinfo/networking/virtual-service-all-v1.yaml
