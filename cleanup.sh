352294ece77355713419aca5e19477acaf371b5f
Jason Morgan <jmorgan@f9vs.com>
2020-07-22 14:32:45 -0400

    Fixed cleanup so it sees istioctl.

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-istio-*/samples/bookinfo/platform/kube/cleanup.sh
+. setup_shell.sh
+
+export NAMESPACE=default && istio-*/samples/bookinfo/platform/kube/cleanup.sh
 istioctl manifest generate --set profile=demo | kubectl delete -f -
 kubectl delete namespace istio-system
